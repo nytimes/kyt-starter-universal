00305bc2cca3a741f7664e1cdee26b2fe66e8c4d
Price <carolyn.price@nytimes.com>
2016-09-10 07:50:53 -0400

    removing unecessary options from kyt config

--- kyt.config.js
+++ kyt.config.js
@@ -3,9 +3,6 @@
 // Edit these properties to make changes.
 
 module.exports = {
-  clientPort: 3001,
-  serverPort: 3000,
-  prototypePort: 3002,
-  debug: false,
   reactHotLoader: true,
+  debug: false,
 };
