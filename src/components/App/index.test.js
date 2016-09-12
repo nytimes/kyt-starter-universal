1bf3111b33e28ed1e0e9bfef61c5b07edf9d810c
Jeremy Gayed <jeremy.gayed@nytimes.com>
2016-09-12 16:23:00 -0400

    Jest tests

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,10 +1,10 @@
 
-import ava from 'ava';
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './';
 
-ava('Test example', (test) => {
+it('Test example', () => {
   const wrapper = shallow(<App />);
-  test.true(wrapper.is('div'));
+  expect(wrapper.is('div')).toBeTruthy();
 });
